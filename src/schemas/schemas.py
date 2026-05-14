dae815175531b46b2223a5c7c7cff16db9ba1b4b
Marat <msaltysh@gmail.com>
2026-05-14 17:50:15 +0500

    Enum

--- src/schemas/schemas.py
+++ src/schemas/schemas.py
@@ -1,7 +1,8 @@
 from datetime import datetime
+from enum import Enum
 from typing import List, Optional
 from uuid import UUID
-from enum import Enum
+
 from pydantic import BaseModel, ConfigDict
 
 
